f80ada396f914a52139e00c9f671746a6de8852f
MohammedK <m.ali.kassem@outlook.com>
2025-03-20 09:41:23 +0100

    test20/3/25

--- Python (secret password generator)/secure_password.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-def generate_password(length=22):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(secrets.choice(characters) for _ in range(length))
-
-print(generate_password())
